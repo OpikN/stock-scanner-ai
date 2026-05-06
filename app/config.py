3a452513496fba10064c917e22d2e77a896c4236
Opik Nuropik <53985304+OpikN@users.noreply.github.com>
2026-05-06 15:12:53 +0700

    Update trade parameters and add advanced management settings

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@
 
 INITIAL_BALANCE = 100_000_000  # 100 juta
 
+
 # =========================
 # RISK MANAGEMENT
 # =========================
@@ -11,9 +12,21 @@ INITIAL_BALANCE = 100_000_000  # 100 juta
 RISK_SAFE = 0.01        # 1%
 RISK_AGGRESSIVE = 0.03  # 3%
 
+
 # =========================
-# TRADE PARAMETER
+# BASIC TRADE PARAMETER
 # =========================
 
 SL_PERCENT = 0.02   # 2% stop loss
-TP_PERCENT = 0.04   # 4% take profit
+TP_PERCENT = 0.04   # fallback TP
+
+
+# =========================
+# ADVANCED TRADE MANAGEMENT 🔥
+# =========================
+
+TP1_PERCENT = 0.02           # partial close
+TP2_PERCENT = 0.04           # final TP
+BREAK_EVEN_TRIGGER = 0.015   # 1.5%
+TRAILING_PERCENT = 0.01      # 1%
+PARTIAL_CLOSE_RATIO = 0.5    # 50%
