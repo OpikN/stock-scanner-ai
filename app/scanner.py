ca3c0553928b5b2cdccaefaeb23eab264245d306
Opik Nuropik <53985304+OpikN@users.noreply.github.com>
2026-05-09 11:51:30 +0700

    Implement live data generation and JSON saving
    Added functionality to generate and save live data in JSON format.

--- app/scanner.py
+++ app/scanner.py
@@ -1,10 +1,64 @@
+import json
+import os
+
+
 def run():
 
     print("[SCANNER START]")
 
+    live_data = {
+
+        "equity": 100021000,
+
+        "floating_pnl": 21000,
+
+        "open_positions": [
+
+            {
+
+                "stock": "BBRI.JK",
+
+                "side": "SELL",
+
+                "entry_price": 3310,
+
+                "current_price": 3260,
+
+                "pnl": 50000,
+
+                "sl": 3343,
+
+                "tp1": 3243
+
+            }
+
+        ]
+
+    }
+
+    os.makedirs(
+        "data",
+        exist_ok=True
+    )
+
+    with open(
+
+        "data/live_data.json",
+
+        "w"
+
+    ) as f:
+
+        json.dump(
+
+            live_data,
+
+            f,
+
+            indent=4
+
+        )
+
     print("[LIVE JSON UPDATED]")
 
     print("[SCANNER FINISHED]")
-
-
-
